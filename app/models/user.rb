1d651616f5ade28b4797bc8fd381863cdbb48d14
coto1130 <mangokotobuki@gmail.com>
2022-01-15 18:06:31 +0900

    ユーザーログイン・ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :lastname, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :lastname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
+  validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
+  validates :birthday, presence: true
+  
   has_many :items
   has_many :purchases
 
